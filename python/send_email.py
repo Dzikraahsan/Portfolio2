3b1eda1119469aebdf9be9f01fac8d71509f4984
Dzikra Ahsan Imawan <ahsandzikra@gmail.com>
2025-07-10 08:34:27 +0700

    update code

--- python/send_email.py
+++ python/send_email.py
@@ -51,5 +51,5 @@ def kirim_email():
         return redirect(url_for('kontak', status='error'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
