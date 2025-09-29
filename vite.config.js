8c6ad20f3eac8bc06ff7b870896f4b44437e1b00
Dzikra Ahsan Imawan <ahsandzikra@gmail.com>
2025-09-29 20:05:41 +0700

    update code

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Config dasar Vite
 export default defineConfig({
     plugins: [react()],
-    build: {
-        outDir: 'dist', // folder hasil build
-    },
 });
